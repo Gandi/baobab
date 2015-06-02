34316d21bac2fd41628226f8ddf5ffacc3b495a5
MoiTux <moitux@gandi.net>
2015-06-03 01:43:24 +0200

    move south migration to django migration

--- baobab/backoffice/migrations/0002_create_services_and_set_timezone_enm.py
+++ baobab/backoffice/migrations/0002_create_services_and_set_timezone_enm.py
@@ -1,129 +1,56 @@
 # -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 import pytz
 
-from south.utils import datetime_utils as datetime
-from south.db import db
-from south.v2 import DataMigration
-from django.db import models
+from django.db import models, migrations
 from django.contrib.auth.models import User
 from django.conf import settings
 
 
 # XXX to avoid to import baobab.backoffice.models.Service
-class Service(object):
-    IAAS = 0
-    PAAS = 1
-    SITE = 2
-    API = 3
-    SSL = 4
-    DOMAIN = 5
-    EMAIL = 6
-    SERVICE_CHOICES = (
-        (IAAS, 'IAAS'),
-        (PAAS, 'PAAS'),
-        (SITE, 'Site'),
-        (API, 'API'),
-        (SSL, 'SSL'),
-        (DOMAIN, 'Domain'),
-        (EMAIL, 'Email'),
-    )
+IAAS = 0
+PAAS = 1
+SITE = 2
+API = 3
+SSL = 4
+DOMAIN = 5
+EMAIL = 6
+SERVICE_CHOICES = (
+    (IAAS, 'IAAS'),
+    (PAAS, 'PAAS'),
+    (SITE, 'Site'),
+    (API, 'API'),
+    (SSL, 'SSL'),
+    (DOMAIN, 'Domain'),
+    (EMAIL, 'Email'),
+)
 
+def forwards(apps, schema_editor):
+    Service = apps.get_model('backoffice', 'Service')
+    TimeZoneEnum = apps.get_model('backoffice', 'TimeZoneEnum')
 
-class Migration(DataMigration):
+    # create service
+    for name, description in SERVICE_CHOICES:
+        Service.objects.create(name=name, description=description)
 
-    def forwards(self, orm):
-        # create service
-        for name, description in Service.SERVICE_CHOICES:
-            orm.Service.objects.create(name=name, description=description)
+    # set timezone
+    for timezone in pytz.common_timezones:
+        TimeZoneEnum.objects.create(name=timezone)
 
-        # set timezone
-        for timezone in pytz.common_timezones:
-            orm.TimeZoneEnum.objects.create(name=timezone)
-        timezone_id = orm.TimeZoneEnum.objects.get(name=settings.TIME_ZONE).id
-        for user in User.objects.all():
-            orm.TimeZone.objects.create(
-                user_id=user.id,
-                timezone_id=timezone_id,
-            )
+    timezone_id = TimeZoneEnum.objects.get(name=settings.TIME_ZONE).id
+    for user in User.objects.all():
+        TimeZone.objects.create(
+            user_id=user.id,
+            timezone_id=timezone_id,
+        )
 
-    def backwards(self, orm):
-        "Write your backwards methods here."
+class Migration(migrations.Migration):
 
-    models = {
-        u'auth.group': {
-            'Meta': {'object_name': 'Group'},
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
-            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
-        },
-        u'auth.permission': {
-            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
-            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
-        },
-        u'auth.user': {
-            'Meta': {'object_name': 'User'},
-            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
-            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
-            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
-            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
-            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
-            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
-            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
-        },
-        u'backoffice.event': {
-            'Meta': {'ordering': "['id', 'pk']", 'object_name': 'Event'},
-            'category': ('django.db.models.fields.PositiveSmallIntegerField', [], {}),
-            'date_end': ('django.db.models.fields.DateTimeField', [], {'default': 'None', 'null': 'True', 'blank': 'True'}),
-            'date_start': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'duration': ('django.db.models.fields.PositiveIntegerField', [], {}),
-            'estimate_date_end': ('django.db.models.fields.DateTimeField', [], {}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'services': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'events'", 'symmetrical': 'False', 'to': u"orm['backoffice.Service']"}),
-            'summary': ('django.db.models.fields.TextField', [], {'default': 'None', 'null': 'True', 'blank': 'True'}),
-            'title': ('django.db.models.fields.CharField', [], {'max_length': '50'})
-        },
-        u'backoffice.eventlog': {
-            'Meta': {'ordering': "['-date']", 'object_name': 'EventLog'},
-            'comment': ('django.db.models.fields.TextField', [], {}),
-            'date': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'event': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'eventlogs'", 'to': u"orm['backoffice.Event']"}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'user': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'eventlogs'", 'to': u"orm['auth.User']"})
-        },
-        u'backoffice.service': {
-            'Meta': {'object_name': 'Service'},
-            'description': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.PositiveSmallIntegerField', [], {'unique': 'True'})
-        },
-        u'backoffice.timezone': {
-            'Meta': {'object_name': 'TimeZone'},
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'timezone': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['backoffice.TimeZoneEnum']"}),
-            'user': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['auth.User']", 'unique': 'True'})
-        },
-        u'backoffice.timezoneenum': {
-            'Meta': {'object_name': 'TimeZoneEnum'},
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
-        },
-        u'contenttypes.contenttype': {
-            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
-            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
-        }
-    }
+    dependencies = [
+        ('backoffice', '0001_initial'),
+    ]
 
-    complete_apps = ['backoffice']
-    symmetrical = True
+    operations = [
+        migrations.RunPython(forwards),
+    ]
--- baobab/translate/migrations/0001_initial.py
+++ baobab/translate/migrations/0001_initial.py
@@ -1,169 +1,100 @@
 # -*- coding: utf-8 -*-
-import datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        # Adding model 'Lang'
-        db.create_table('translate_lang', (
-            ('id', self.gf('django.db.models.fields.AutoField')
-             (primary_key=True)),
-            ('name', self.gf('django.db.models.fields.CharField')
-             (unique=True, max_length=200)),
-            ('iso', self.gf('django.db.models.fields.CharField')
-             (unique=True, max_length=5)),
-        ))
-        db.send_create_signal('translate', ['Lang'])
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('backoffice', '0002_create_services_and_set_timezone_enm'),
+    ]
 
-        # Adding model 'EventData'
-        db.create_table('translate_eventdata', (
-            ('id', self.gf('django.db.models.fields.AutoField')
-             (primary_key=True)),
-            ('event', self.gf('django.db.models.fields.related.ForeignKey')
-             (related_name='eventdatas', to=orm['backoffice.Event'])),
-            ('lang', self.gf('django.db.models.fields.related.ForeignKey')
-             (related_name='eventdatas', to=orm['translate.Lang'])),
-            ('title', self.gf('django.db.models.fields.CharField')
-             (max_length=255)),
-            ('summary', self.gf('django.db.models.fields.TextField')
-             (default=None, null=True, blank=True)),
-            ('user', self.gf('django.db.models.fields.related.ForeignKey')(
-                default=None, related_name='eventdatas', null=True, to=orm['auth.User'])),
-        ))
-        db.send_create_signal('translate', ['EventData'])
-
-        # Adding unique constraint on 'EventData', fields ['event', 'lang']
-        db.create_unique('translate_eventdata', ['event_id', 'lang_id'])
-
-        # Adding model 'EventLogData'
-        db.create_table('translate_eventlogdata', (
-            ('id', self.gf('django.db.models.fields.AutoField')
-             (primary_key=True)),
-            ('eventlog', self.gf('django.db.models.fields.related.ForeignKey')
-             (related_name='eventlogdatas', to=orm['backoffice.EventLog'])),
-            ('lang', self.gf('django.db.models.fields.related.ForeignKey')
-             (related_name='eventlogdatas', to=orm['translate.Lang'])),
-            ('comment', self.gf('django.db.models.fields.TextField')()),
-            ('user', self.gf('django.db.models.fields.related.ForeignKey')(
-                default=None, related_name='eventlogdatas', null=True, to=orm['auth.User'])),
-        ))
-        db.send_create_signal('translate', ['EventLogData'])
-
-        # Adding unique constraint on 'EventLogData', fields ['eventlog',
-        # 'lang']
-        db.create_unique('translate_eventlogdata', ['eventlog_id', 'lang_id'])
-
-    def backwards(self, orm):
-        # Removing unique constraint on 'EventLogData', fields ['eventlog',
-        # 'lang']
-        db.delete_unique('translate_eventlogdata', ['eventlog_id', 'lang_id'])
-
-        # Removing unique constraint on 'EventData', fields ['event', 'lang']
-        db.delete_unique('translate_eventdata', ['event_id', 'lang_id'])
-
-        # Deleting model 'Lang'
-        db.delete_table('translate_lang')
-
-        # Deleting model 'EventData'
-        db.delete_table('translate_eventdata')
-
-        # Deleting model 'EventLogData'
-        db.delete_table('translate_eventlogdata')
-
-    models = {
-        'auth.group': {
-            'Meta': {'object_name': 'Group'},
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
-            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
-        },
-        'auth.permission': {
-            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
-            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
-        },
-        'auth.user': {
-            'Meta': {'object_name': 'User'},
-            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
-            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
-            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
-            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
-            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
-            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
-            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
-        },
-        'backoffice.event': {
-            'Meta': {'ordering': "['id', 'pk']", 'object_name': 'Event'},
-            'category': ('django.db.models.fields.PositiveSmallIntegerField', [], {}),
-            'date_end': ('django.db.models.fields.DateTimeField', [], {'default': 'None', 'null': 'True', 'blank': 'True'}),
-            'date_start': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'duration': ('django.db.models.fields.PositiveIntegerField', [], {}),
-            'estimate_date_end': ('django.db.models.fields.DateTimeField', [], {}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'msg': ('django.db.models.fields.CharField', [], {'default': 'None', 'max_length': '255', 'null': 'True', 'blank': 'True'}),
-            'msg_id': ('django.db.models.fields.CharField', [], {'default': 'None', 'max_length': '30', 'null': 'True'}),
-            'services': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'events'", 'symmetrical': 'False', 'to': "orm['backoffice.Service']"}),
-            'summary': ('django.db.models.fields.TextField', [], {'default': 'None', 'null': 'True', 'blank': 'True'}),
-            'title': ('django.db.models.fields.CharField', [], {'max_length': '255'})
-        },
-        'backoffice.eventlog': {
-            'Meta': {'ordering': "['-date']", 'object_name': 'EventLog'},
-            'comment': ('django.db.models.fields.TextField', [], {}),
-            'date': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'event': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'eventlogs'", 'to': "orm['backoffice.Event']"}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'msg': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
-            'msg_id': ('django.db.models.fields.CharField', [], {'default': 'None', 'max_length': '30', 'null': 'True'}),
-            'user': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'eventlogs'", 'to': "orm['auth.User']"})
-        },
-        'backoffice.service': {
-            'Meta': {'object_name': 'Service'},
-            'description': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.PositiveSmallIntegerField', [], {'unique': 'True'})
-        },
-        'contenttypes.contenttype': {
-            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
-            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
-        },
-        'translate.eventdata': {
-            'Meta': {'unique_together': "(('event', 'lang'),)", 'object_name': 'EventData'},
-            'event': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'eventdatas'", 'to': "orm['backoffice.Event']"}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'lang': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'eventdatas'", 'to': "orm['translate.Lang']"}),
-            'summary': ('django.db.models.fields.TextField', [], {'default': 'None', 'null': 'True', 'blank': 'True'}),
-            'title': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
-            'user': ('django.db.models.fields.related.ForeignKey', [], {'default': 'None', 'related_name': "'eventdatas'", 'null': 'True', 'to': "orm['auth.User']"})
-        },
-        'translate.eventlogdata': {
-            'Meta': {'unique_together': "(('eventlog', 'lang'),)", 'object_name': 'EventLogData'},
-            'comment': ('django.db.models.fields.TextField', [], {}),
-            'eventlog': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'eventlogdatas'", 'to': "orm['backoffice.EventLog']"}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'lang': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'eventlogdatas'", 'to': "orm['translate.Lang']"}),
-            'user': ('django.db.models.fields.related.ForeignKey', [], {'default': 'None', 'related_name': "'eventlogdatas'", 'null': 'True', 'to': "orm['auth.User']"})
-        },
-        'translate.lang': {
-            'Meta': {'object_name': 'Lang'},
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'iso': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '5'}),
-            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '200'})
-        }
-    }
-
-    complete_apps = ['translate']
+    operations = [
+        migrations.CreateModel(
+            name='EventData',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(help_text=b'A short description of the event', max_length=255)),
+                ('summary', models.TextField(default=None, null=True, blank=True)),
+                ('event', models.ForeignKey(related_name='eventdatas', to='backoffice.Event')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='EventLogData',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('comment', models.TextField()),
+                ('eventlog', models.ForeignKey(related_name='eventlogdatas', to='backoffice.EventLog')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Lang',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(unique=True, max_length=200)),
+                ('iso', models.CharField(unique=True, max_length=5)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='eventlogdata',
+            name='lang',
+            field=models.ForeignKey(related_name='eventlogdatas', to='translate.Lang'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='eventlogdata',
+            name='user',
+            field=models.ForeignKey(related_name='eventlogdatas', default=None, to=settings.AUTH_USER_MODEL, null=True),
+            preserve_default=True,
+        ),
+        migrations.AlterUniqueTogether(
+            name='eventlogdata',
+            unique_together=set([('eventlog', 'lang')]),
+        ),
+        migrations.AddField(
+            model_name='eventdata',
+            name='lang',
+            field=models.ForeignKey(related_name='eventdatas', to='translate.Lang'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='eventdata',
+            name='user',
+            field=models.ForeignKey(related_name='eventdatas', default=None, to=settings.AUTH_USER_MODEL, null=True),
+            preserve_default=True,
+        ),
+        migrations.AlterUniqueTogether(
+            name='eventdata',
+            unique_together=set([('event', 'lang')]),
+        ),
+        migrations.CreateModel(
+            name='Event',
+            fields=[
+            ],
+            options={
+                'proxy': True,
+            },
+            bases=('backoffice.event',),
+        ),
+        migrations.CreateModel(
+            name='EventLog',
+            fields=[
+            ],
+            options={
+                'proxy': True,
+            },
+            bases=('backoffice.eventlog',),
+        ),
+    ]
