31e939e362a855bcd12836b8de0bfa394e9c314a
Grigouze <grigouze@yahoo.fr>
2015-06-21 11:05:02 +0200

    Some improvments for testing stuff
    - rename tests files with prefix "test_" (better for py.test)
    - add support for travis CI
    - add support for coveralls

--- baobab/backoffice/tests/__init__.py
+++ baobab/backoffice/tests/__init__.py
@@ -5,5 +5,5 @@ django only look for test in app_name.tests.py or app_name.tests/__init__.py
 add the tests classes here to make them visible to django
 """
 
-from .event import TestEvent
-from .status import TestStatus
+from .test_event import TestEvent
+from .test_status import TestStatus
--- baobab/rss/tests/__init__.py
+++ baobab/rss/tests/__init__.py
@@ -5,4 +5,4 @@ django only look for test in app_name.tests.py or app_name.tests/__init__.py
 add the tests classes here to make them visible to django
 """
 
-from .rss import TestRss
+from .test_rss import TestRss
